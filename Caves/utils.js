712f481ec50733972e54b41c6a4847e3e58072ac
jough13 <joughdonakowski@gmail.com>
2026-02-01 14:27:39 -0500

    Update utils.js

--- Caves/utils.js
+++ Caves/utils.js
@@ -167,3 +167,10 @@ const elevationNoise = Object.create(Perlin);
 elevationNoise.init(WORLD_SEED + ':elevation');
 const moistureNoise = Object.create(Perlin);
 moistureNoise.init(WORLD_SEED + ':moisture');
+
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
