2948c0a8f721e7a1476dd5c15f0041d040d6f7a6
jough13 <joughdonakowski@gmail.com>
2026-02-07 15:00:13 -0500

    Update state.js

--- Caves/state.js
+++ Caves/state.js
@@ -1,1 +1,19 @@
-state.js
+let player_id;
+let playerRef;
+let onlinePlayerRef;
+let otherPlayers = {};
+let unsubscribePlayerListener;
+let worldStateListeners = {};
+
+let isProcessingMove = false; 
+
+let lastAiExecution = 0;
+
+let cachedThemeColors = {};
+
+const processingSpawnTiles = new Set();
+
+let areGlobalListenersInitialized = false;
+
+// --- OPTIMIZATION GLOBALS ---
+let saveTimeout = null; // Tracks the pending save timer
