43d8e01044bcdab58f978dc08e86b0007c6d0bd2
jough13 <joughdonakowski@gmail.com>
2026-02-07 15:45:07 -0500

    Create ui.js

---
+++ Caves/ui.js
@@ -0,0 +1,10 @@
+// DOM Element Selectors
+const characterSelectModal = document.getElementById('characterSelectModal');
+const slotsContainer = document.getElementById('slotsContainer');
+const logoutFromSelectButton = document.getElementById('logoutFromSelectButton');
+let currentUser = null; // Store the firebase user object
+
+const charCreationModal = document.getElementById('charCreationModal');
+const timeDisplay = document.getElementById('timeDisplay');
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
