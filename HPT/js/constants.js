06a19b6e41811a366473676272d3ef62003c902c
jough13 <joughdonakowski@gmail.com>
2026-02-09 14:55:51 -0500

    Update constants.js

--- HPT/js/constants.js
+++ HPT/js/constants.js
@@ -2,8 +2,8 @@
 // --- APPLICATION METADATA & CONSTANTS
 //==============================================================================
 
-const VERSION = '0.8.7';
-const LAST_UPDATED = 'February 6th, 2026';
+const VERSION = '0.8.8';
+const LAST_UPDATED = 'February 9th, 2026';
 
 // Global constant for all navigation views
 
