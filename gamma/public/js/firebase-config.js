f6ac7514f58bde19dc9118d5a92b7080639237e3
Jough Donakowski <joughdonakowski@gmail.com>
2026-04-20 08:38:14 -0400

    Create firebase-config.js

---
+++ gamma/public/js/firebase-config.js
@@ -0,0 +1,25 @@
+// public/js/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { initializeFirestore, persistentLocalCache } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCxSXC52M4M176X_U_s5z6tDZsJbyXCdkM",
+    authDomain: "radiography-tracker.firebaseapp.com",
+    projectId: "radiography-tracker",
+    storageBucket: "radiography-tracker.firebasestorage.app",
+    messagingSenderId: "124869239575",
+    appId: "1:124869239575:web:c90d4e0b688abab448cd52",
+    measurementId: "G-XRJWGPV6E5"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Using the simplified cache to prevent deadlocks
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache() 
+});
+
+export const storage = getStorage(app);
+export const auth = getAuth(app);
