e2a16c862b755b1fe31c6a2f188220e0022beb65
Jough Donakowski <joughdonakowski@gmail.com>
2026-04-30 10:03:31 -0400

    Update sw.js

--- NORM/sw.js
+++ NORM/sw.js
@@ -1,1 +1,24 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'shipassist-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install the Service Worker and Cache Files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Intercept network requests and serve from cache if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
