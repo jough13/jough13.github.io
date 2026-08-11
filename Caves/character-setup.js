a857e66616122836f7247a2215459b425773e8a7
Jough Donakowski <joughdonakowski@gmail.com>
2026-08-10 21:01:03 -0400

    Refactor character setup by removing unused code

--- Caves/character-setup.js
+++ Caves/character-setup.js
@@ -161,9 +161,7 @@ window.selectSlot = async function (slotId) {
     if (isEnteringGame) return; 
     isEnteringGame = true;
 
-    window.currentSlotId = slotId;
-
-    // JUICE WIN: Play a deep, resonant magical tone when tethering to a timeline
+    // Play a deep, resonant magical tone when tethering to a timeline
     if (typeof AudioSystem !== 'undefined') {
         AudioSystem.playTone(200, 'sine', 0.5, 0.2, false, 100);
     }
