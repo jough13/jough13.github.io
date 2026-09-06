df0784ebe433b28ed79e99e45ae1d5b784922ec1
Jough Donakowski <joughdonakowski@gmail.com>
2026-09-06 10:45:50 -0400

    Add yellow color support in text formatting

--- Caves/ui.js
+++ Caves/ui.js
@@ -146,6 +146,7 @@ const FORMAT_REGEXES = [
     { rx: /{green:(.*?)}/g, repl: '<span class="text-green-500 font-bold drop-shadow-md">$1</span>' },
     { rx: /{blue:(.*?)}/g, repl: '<span class="text-blue-400 font-bold drop-shadow-md">$1</span>' },
     { rx: /{gold:(.*?)}/g, repl: '<span class="text-yellow-500 font-bold drop-shadow-md">$1</span>' },
+    { rx: /{yellow:(.*?)}/g, repl: '<span class="text-yellow-400 font-bold drop-shadow-md">$1</span>' },
     { rx: /{purple:(.*?)}/g, repl: '<span class="text-purple-400 font-bold drop-shadow-md">$1</span>' },
     { rx: /{cyan:(.*?)}/g, repl: '<span class="text-cyan-400 font-bold drop-shadow-md">$1</span>' },
     { rx: /{orange:(.*?)}/g, repl: '<span class="text-orange-400 font-bold drop-shadow-md">$1</span>' },
